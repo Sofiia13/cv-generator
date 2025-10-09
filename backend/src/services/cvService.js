637c4d3126934d49abef54bb12bea7a8318cce4b
Sofiia13 <sophiyastanish@gmail.com>
2025-10-09 16:22:30 +0300

    change formatting of generated file

--- backend/src/services/cvService.js
+++ backend/src/services/cvService.js
@@ -1,14 +1,42 @@
-import { Document, Packer, Paragraph, TextRun } from "docx";
+import { Document, Packer, Paragraph, TextRun, AlignmentType } from "docx";
 
 export const createDocx = async ({ name, city, skills, experience }) => {
+  const skillsArray = skills
+    ? skills.split(",").map((skill) => skill.trim())
+    : [];
+
   const doc = new Document({
     sections: [
       {
         properties: {},
         children: [
-          new Paragraph({ text: `Name: ${name}`, heading: "heading1" }),
+          new Paragraph({
+            children: [
+              new TextRun({
+                text: "Resume",
+                bold: true,
+                size: 72,
+              }),
+            ],
+            alignment: "center",
+            spacing: { before: 300, after: 300 },
+          }),
+
+          new Paragraph({
+            text: `Name: ${name}`,
+            heading: "heading1",
+          }),
           new Paragraph({ text: `City: ${city}` }),
-          new Paragraph({ text: `Skills: ${skills}` }),
+
+          new Paragraph({ text: `Skills:`, heading: "heading2" }),
+          ...skillsArray.map(
+            (skill) =>
+              new Paragraph({
+                text: skill,
+                bullet: { level: 0 },
+              })
+          ),
+
           new Paragraph({ text: `Experience: ${experience}` }),
         ],
       },
