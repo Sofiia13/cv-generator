b4a03b71089bc336a5ad687e39ae835289ba6973
Sofiia13 <sophiyastanish@gmail.com>
2025-10-09 12:06:19 +0300

    add state to the form

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,13 +1,38 @@
 import type React from "react";
 import { Input } from "./Input";
+import { useState } from "react";
+
+type FormData = {
+  name: string;
+  position: string;
+  city: string;
+  skills: string;
+};
 
 export const Form: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    position: "",
+    city: "",
+    skills: "",
+  });
+
+  const handleChange = () => {};
+
+  const handleSubmit = () => {};
+
   return (
     <>
-      <form action="">
-        <Input type="text" label="Name" />
-        <Input type="text" label="City"/>
-        <Input type="text" label="Skills"/>
+      <form onSubmit={handleSubmit}>
+        <Input type="text" label="Name" value={formData.name} onChange="" />
+        <Input
+          type="text"
+          label="Position"
+          value={formData.position}
+          onChange=""
+        />
+        <Input type="text" label="City" value={formData.city} onChange="" />
+        <Input type="text" label="Skills" value={formData.skills} onChange="" />
       </form>
     </>
   );
