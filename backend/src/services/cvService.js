b2ac41cca02d11ba5684a6dc6c3e058d3f0829e7
Sofiia13 <sophiyastanish@gmail.com>
2025-10-09 11:55:27 +0300

    change function

--- backend/src/services/cvService.js
+++ backend/src/services/cvService.js
@@ -1,13 +1,13 @@
 import * as fs from "fs";
 import { Document, Packer, Paragraph, TextRun } from "docx";
 
-export const generateCV = async ({ name, city, skills, experience }) => {
+export const createDocx = async ({ name, city, skills, experience }) => {
   const doc = new Document({
     sections: [
       {
         properties: {},
         children: [
-          new Paragraph({ text: `Name: ${name}`, heading: "Heading1" }),
+          new Paragraph({ text: `Name: ${name}`, heading: "heading1" }),
           new Paragraph({ text: `City: ${city}` }),
           new Paragraph({ text: `Skills: ${skills}` }),
           new Paragraph({ text: `Experience: ${experience}` }),
@@ -16,7 +16,6 @@ export const generateCV = async ({ name, city, skills, experience }) => {
     ],
   });
 
-  Packer.toBuffer(doc).then((buffer) => {
-    fs.writeFileSync("My Document.docx", buffer);
-  });
+  const buffer = await Packer.toBuffer(doc);
+  return buffer;
 };
